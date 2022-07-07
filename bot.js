0f60b19168fba367f0562c17df638ce8a6c82588
w2lody <44412859+w2lody@users.noreply.github.com>
2022-07-07 04:01:52 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,10 +2,10 @@ const Discord = require('discord.js'),
     bot = new Discord.Client({sisableEveryone: true})
     console.log("muuuuuuuuute,");
 bot.on('guildMemberAdd', member => {
-const mohamed= member.guild.channels.get("535229239306354698");
+const mohamed= member.guild.channels.get("617152221070491658");
 if(!mohamed) return;
 if(mohamed) {
-setTimeout(() => mohamed.send(`**- Hoşgeldiniz To Mlk. :umbrella:**`), 4000)        
+setTimeout(() => mohamed.send(`**- Hoşgeldiniz To United. :umbrella:**`), 4000)        
 }
 });
 
